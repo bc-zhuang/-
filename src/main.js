a60fa6d48da67aa4f90f6e79ed9fe03528ecd9bd
huanhuan <377235460@qq.com>
2023-05-16 13:44:44 +0800

    Revert "Revert "first commit""
    This reverts commit 35ac3b0abec008a085f2f42a9d2acb5e9f228d32.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from "./App.vue";
 
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+import Router from "./router/index";
+import axios from "axios";
+Vue.prototype.$axios =axios;
+
+import '@/assets/css/global.css'
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router:Router
 }).$mount('#app')
+
